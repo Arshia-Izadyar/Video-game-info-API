385fbb8c77cd6f5d2765f26ff4dc688899be562c
Arshia-Izadyar <arshiaa104@gmail.com>
2023-08-05 18:22:19 +0330

    added category view

--- games/urls.py
+++ games/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
 
-from .views import GamesListView, GameDetailView
+from .views import GamesListView, GameDetailView, CategoryListView, TopGamesListView
 
 app_name = "game"
 
 urlpatterns = [
     path("list/", GamesListView.as_view(), name="list"),
     path("detail/<int:pk>/", GameDetailView.as_view(), name="detail"),
+    path("category/<slug:genre>/", CategoryListView.as_view(), name="genre-list"),
+    path("top250/", TopGamesListView.as_view(), name="top250"),
 ]
--- games/views.py
+++ games/views.py
@@ -2,6 +2,7 @@ from rest_framework.generics import ListAPIView, RetrieveUpdateAPIView
 from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
+from django.db.models import Q
 
 
 from .models import Game
@@ -27,3 +28,20 @@ class GameDetailView(RetrieveUpdateAPIView):
         return Game.objects.filter(pk=pk) 
     
     
+class TopGamesListView(ListAPIView):
+    serializer_class = GamesSerializer
+    permission_classes = [IsAuthenticated]
+    queryset = Game.objects.filter(Q(must_play=True) & Q(score__gte=80))[:250]
+    pagination_class = StandardPagination
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['genre', 'company', 'platform']
+    search_fields = ['title']
+    ordering_fields = ["score", "release_date"]
+    
+class CategoryListView(ListAPIView):
+    serializer_class = GamesSerializer
+    permission_classes = [IsAuthenticated]
+    def get_queryset(self):
+        genre = self.kwargs.get("genre")
+        return Game.objects.filter(genre__name__icontains=genre)
+
